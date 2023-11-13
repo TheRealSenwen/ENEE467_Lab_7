558db272e6fc0fb9e4eadb510ae34c2d9c554eb5
wliu1215 <wliu1215@terpmail.umd.edu>
2023-11-13 14:54:28 -0500

    Largest square

--- src/ur3e_trajectory/src/circle.cpp
+++ src/ur3e_trajectory/src/circle.cpp
@@ -1,4 +1,5 @@
 #include "../include/circle.hpp"
+#include <cmath>
 
 int main(int argc, char **argv)
 {
@@ -24,6 +25,88 @@ int main(int argc, char **argv)
     moveit::planning_interface::MoveGroupInterface arm_move_group("manipulator");
 
     //Write your code for following the circle trajectory here.
-     
+    
+    // Create instance of pose target plan
+    moveit::planning_interface::MoveGroupInterface::Plan pose_plan;
+
+    geometry_msgs::Pose pose_target;
+    pose_target.position.x = 0.0;
+    pose_target.position.y = -0.18;
+    pose_target.position.z = 1.4;
+    pose_target.orientation.x = 0.0;
+    pose_target.orientation.y = 0.707;
+    pose_target.orientation.z = 0.0;
+    pose_target.orientation.w = 0.707;
+
+    bool pose_plan_success;
+    std::string reference_frame = "world";
+    pose_plan_success = ArmController::planToPoseTarget(planning_options, arm_move_group, pose_target, reference_frame, pose_plan);
+
+    if(pose_plan_success){
+        ROS_INFO("Moving to pose target");
+        arm_move_group.execute(pose_plan);
+    }
+
+    // Get the start Pose
+    geometry_msgs::Pose start_pose = arm_move_group.getCurrentPose().pose;
+    //start_pose.position.z -= 0.77;
+
+    geometry_msgs::Pose end_pose = start_pose;
+#include <cmath>
+    // Define waypoints for cartesian path
+    std::vector<geometry_msgs::Pose> waypoints;
+
+    double radius = 0.05;
+    double x_origin = pose_target.position.x;
+    double y_origin = pose_target.position.y;
+    double z_origin = pose_target.position.z;
+    double x_coord, y_coord, z_coord;
+
+    int number_of_points = 20;
+    int plane = 2; // 1: XY, 2: XZ, 3: YZ
+
+    for(int i = 0; i < number_of_points; i++){
+        if(plane == 1){
+            x_coord = x_origin + radius * cos(i*2*M_PI/number_of_points);
+            y_coord = y_origin + radius * sin(i*2*M_PI/number_of_points);
+
+            end_pose.position.x = x_coord;
+            end_pose.position.y = y_coord;
+            end_pose.position.z += 0.0;
+
+            waypoints.push_back(end_pose);
+        }
+
+        if(plane == 2){
+            x_coord = x_origin + radius * cos(i*2*M_PI/number_of_points);
+            z_coord = z_origin + radius * sin(i*2*M_PI/number_of_points);
+            std::cout << x_coord << "," << z_coord << std::endl;
+
+            end_pose.position.x = x_coord;
+            end_pose.position.y += 0.0;
+            end_pose.position.z = z_coord;
+
+            waypoints.push_back(end_pose);
+        }
+
+        if(plane == 3){
+            y_coord = y_origin + radius * cos(i*2*M_PI/number_of_points);
+            z_coord = z_origin + radius * sin(i*2*M_PI/number_of_points);
+
+            end_pose.position.x += 0.0;
+            end_pose.position.y = y_coord;
+            end_pose.position.z = z_coord;
+
+            waypoints.push_back(end_pose);
+        }
+        
+    }
+
+    moveit_msgs::RobotTrajectory trajectory;
+    trajectory = ArmController::planCartesianPath(start_pose, waypoints, reference_frame, arm_move_group);
+
+    n.setParam("/record_pose", true);
+    arm_move_group.execute(trajectory);
+    n.setParam("/record_pose", false);
     
 }
--- src/ur3e_trajectory/src/square.cpp
+++ src/ur3e_trajectory/src/square.cpp
@@ -1,4 +1,5 @@
 #include "../include/square.hpp"
+#include <cmath>
 
 int main(int argc, char **argv)
 {
@@ -24,44 +25,80 @@ int main(int argc, char **argv)
     moveit::planning_interface::MoveGroupInterface arm_move_group("manipulator");
 
     //Write your code for following the square trajectory here.
+    std::string reference_frame = "world";
+
+    // Create instance of joint target plan
+    /*
+    moveit::planning_interface::MoveGroupInterface::Plan joint_plan;
+
+    std::map<std::string, double> joint_targets;
+    joint_targets["elbow_joint"] = 0.08*2;
+    joint_targets["shoulder_lift_joint"] = -0.08;
+    joint_targets["shoulder_pan_joint"] = 1.57;
+    joint_targets["wrist_1_joint"] = -1.57-0.08;
+    joint_targets["wrist_2_joint"] = 0.0;
+    joint_targets["wrist_3_joint"] = 0.0;
+
+    bool joint_plan_success;
+    joint_plan_success = ArmController::planToJointTargets(planning_options, arm_move_group, joint_plan, joint_targets);
+
+    if(joint_plan_success){
+        ROS_INFO("Moving to joint target");
+        arm_move_group.execute(joint_plan);
+    }
+    */
 
     // Create instance of pose target plan
+    
     moveit::planning_interface::MoveGroupInterface::Plan pose_plan;
 
     geometry_msgs::Pose pose_target;
     pose_target.position.x = 0.3;
     pose_target.position.y = 0.3;
-    pose_target.position.z = 1.0;
+    pose_target.position.z = 1.3;
     pose_target.orientation.x = 0.0;
     pose_target.orientation.y = 0.0;
     pose_target.orientation.z = 0.0;
     pose_target.orientation.w = 0.0;
 
     bool pose_plan_success;
-    std::string reference_frame = "base_link";
     pose_plan_success = ArmController::planToPoseTarget(planning_options, arm_move_group, pose_target, reference_frame, pose_plan);
 
     if(pose_plan_success){
         ROS_INFO("Moving to pose target");
         arm_move_group.execute(pose_plan);
     }
-
-    double path_x[] = {0.2, 0.2, 0.3, 0.3};
-    double path_y[] = {0.3, 0.2, 0.2, 0.3};
+    
 
     // Get the start Pose
     geometry_msgs::Pose start_pose = arm_move_group.getCurrentPose().pose;
-    start_pose.position.z -= 0.77;
+    double z0 = start_pose.position.z;
 
-    geometry_msgs::Pose end_pose = start_pose;
+    // Generate square path
+    double square_path_x[] = {0.2, 0.2, 0.3, 0.3};
+    double square_path_y[] = {0.3, 0.2, 0.2, 0.3};
+    double square_path_z[] = {z0, z0, z0, z0};
+
+    // Generate path for largest possible square
+    double L = 0.4*cos(atan(2));
+    double tilt = 0.02;
+
+    double largest_path_x[] = {L, L, -L, -L};
+    double largest_path_y[] = {0.0, 2*L*cos(tilt), 2*L*cos(tilt), 0.0};
+    double largest_path_z[] = {z0, z0+L*sin(tilt), z0+L*sin(tilt), z0};
 
     // Define waypoints for cartesian path
+    geometry_msgs::Pose end_pose = start_pose;
     std::vector<geometry_msgs::Pose> waypoints;
+
+    double* path_x = largest_path_x;
+    double* path_y = largest_path_y;
+    double* path_z = largest_path_z;
 
     for(int i = 0; i < 4; i++){
         end_pose.position.x = path_x[i];
         end_pose.position.y = path_y[i];
-        end_pose.position.z += 0.0;
+        end_pose.position.z = path_z[i];
         waypoints.push_back(end_pose);
     }
 
@@ -71,5 +108,4 @@ int main(int argc, char **argv)
     n.setParam("/record_pose", true);
     arm_move_group.execute(trajectory);
     n.setParam("/record_pose", false);
-
 }
